a7e5e858f44bcc7299938de5308a92c500afd370
Su Ho V <hovansu.it@gmail.com>
2017-06-23 14:40:23 +0700

    - ui test for project

---
+++ FoodAndFitnessUITests/View/Controllers/SignupControllerUITests.swift
@@ -0,0 +1,26 @@
+//
+//  SignupControllerUITests.swift
+//  FoodAndFitness
+//
+//  Created by Mylo Ho on 6/23/17.
+//  Copyright © 2017 SuHoVan. All rights reserved.
+//
+
+import XCTest
+
+class SignupControllerUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testAction() {
+        let _ = XCUIApplication()
+    }
+}
